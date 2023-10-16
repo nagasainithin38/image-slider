e67df7da22f40f89c31f17f90c4e20aeb66a1960
nagasainithin38 <NagaSaiNithin.Kristam@Chubb.com>
2023-10-16 13:09:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'image-slider';
+  title = 'carousel';
+  images:Array<string>=['https://api.slingacademy.com/public/sample-photos/1.jpeg',
+        'https://api.slingacademy.com/public/sample-photos/2.jpeg',
+        'https://api.slingacademy.com/public/sample-photos/3.jpeg',
+        'https://api.slingacademy.com/public/sample-photos/4.jpeg'
+      ]
+  index:number=0;
+
+  decrease(){
+    this.index-=1
+    if (this.index==-1){
+      this.index=3;
+    }
+  }
+  increase(){
+    this.index+=1
+    if (this.index==4){
+      this.index=0;
+    }
+  }
+  changeImage(idx:number){
+    this.index=idx;
+  }
 }
